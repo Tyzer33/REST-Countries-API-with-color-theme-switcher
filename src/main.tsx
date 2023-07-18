a472339536ce08962438fdd05ad94c394692822f
Tyzer33 <78021579+Tyzer33@users.noreply.github.com>
2023-07-18 11:27:11 +0200

    Added ErrorBoundary
    Added ErrorBoundary just for the rest of the errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ErrorBoundary } from 'react-error-boundary'
 import Header from './components/Header/Header'
 import { DataProvider } from './context/DataContext'
 import CountrySelection from './components/Country Selection/CountrySelection'
 import CountryInfoPage from './components/Country Info Page/CountryInfoPage'
 import GlobalStyle from './styles/globalStyles'
 import { CustomThemeProvider } from './context/CustomThemeContext'
+import ErrorMessage from './components/ErrorMessage/ErrorMessage'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <CustomThemeProvider>
-      <GlobalStyle />
-      <BrowserRouter>
-        <Header />
-        <DataProvider>
-          <Routes>
-            <Route path="/" element={<CountrySelection />} />
-            <Route path="/:country" element={<CountryInfoPage />} />
-          </Routes>
-        </DataProvider>
-      </BrowserRouter>
-    </CustomThemeProvider>
+    <ErrorBoundary fallback={<ErrorMessage />}>
+      <CustomThemeProvider>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Header />
+          <DataProvider>
+            <Routes>
+              <Route path="/" element={<CountrySelection />} />
+              <Route path="/:country" element={<CountryInfoPage />} />
+            </Routes>
+          </DataProvider>
+        </BrowserRouter>
+      </CustomThemeProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 )
